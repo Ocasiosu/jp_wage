38fa302c7586e4f208400a2566662048d82cbf44
wanko <sokubupa@honeys.be>
2022-02-03 14:58:13 +0900

    3rd

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pydeck as pdk
 import plotly.express as px
 
-st.title('日本の賃金データのダッシュボード')
+st.title('日本の賃金データ（2010-2019）をさまざまなグラフで可視化')
 
 df_jp_ind = pd.read_csv('./雇用_医療福祉_一人当たり賃金_全国_全産業.csv',encoding='shift_jis')
 df_jp_category = pd.read_csv('./雇用_医療福祉_一人当たり賃金_全国_大分類.csv',encoding='shift_jis')
